73ad30edf095bbd9c35968c5e4eddd4bd00361a3
Dmitry Weiner <dmitry.weiner@gmail.com>
2021-11-28 13:41:00 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+    src: path.resolve(__dirname, './src'), // source files
+    build: path.resolve(__dirname, './build'), // production build files
+    static: path.resolve(__dirname, './public'), // static files to copy to build folder
+};
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: [paths.src + '/index.js'],
+    output: {
+        path: paths.build,
+        filename: '[name].bundle.js',
+        publicPath: '/',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: paths.static,
+                    to: paths.build,
+                    globOptions: {
+                        ignore: ['**/index.html']
+                    }
+                }
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: paths.static + '/index.html', // template file
+            filename: 'index.html', // output file
+            inject: true
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ]
+    }
+};
